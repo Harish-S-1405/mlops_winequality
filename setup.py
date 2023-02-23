60f5fea4854bcecd8d1440aaae9ca06a0e857a37
harish <harish.s@dhiomics.com>
2023-02-23 11:52:52 +0530

    latest

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(    
+     name="src",
+     version='0.0.1',
+     description='its mlops wineq package',
+     author= 'HarishS',
+     packages=find_packages(),
+     license='MIT'
+
+)
+
